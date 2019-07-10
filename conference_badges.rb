5648ec078cda548d1fe15b921fbc34af90e48a1d
Chris Cheong <chrisxcheong@gmail.com>
2019-07-10 04:32:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,8 @@ end
 def batch_badge_creator(array)
   array.map { |name| badge_maker(name)}
 end
+
+def assign_rooms(array)
+  array.each_with_index { |name| "Hello, #{name}! You'll be assigned to room #{i+1}!" }
+
+end
